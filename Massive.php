d861b09edd53ed9f2a28c317d5ee619dc7e605cc
Vlad <furdanno@mail.ru>
2020-09-30 20:27:00 +0300

    Функция считает идут ли числа в массиве по возрастанию

--- Massive.php
+++ Massive.php
@@ -3,6 +3,21 @@
 //Begin
 function isContinuousSequence(array $mas)
 {
-
+    if ($mas === []) {
+        return false;
+    } else {
+        $hz = 0;
+        $a = 0;
+        foreach ($mas as $index => $numb) {
+            $id = $numb - $index;
+            $hz = $hz + $id;
+            $a = $a + 1;
+        }
+        if ($hz / $a !== $id || $a === 1) {
+            return false;
+        } else {
+            return true;
+        }
+    }
 }
 
