7a82bbd36bb22fbf1584c8030b3067ee8c506e3e
Vlad <furdanno@mail.ru>
2020-10-01 11:36:59 +0300

    Функция принимает на вход в виде массива кошелёк с деньгами и название валюты и возвращает сумму денег указанной валюты

--- Br_c.php
+++ Br_c.php
@@ -14,9 +14,3 @@ function getTotalAmount(array $money, $nom)
     }
     return $sum;
 }
-
-$money1 = ['eur 10', 'usd 1', 'usd 10', 'rub 50', 'usd 5', 'rub 10'];
-$money2 = ['eur 10', 'usd 1', 'eur 5', 'rub 100', 'eur 20', 'eur 100', 'rub 200'];
-$money3 = ['eur 10', 'rub 50', 'eur 5', 'rub 10', 'rub 10', 'eur 100', 'rub 200'];
-
-print_r(getTotalAmount($money3, 'rub'));
