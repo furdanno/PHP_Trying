3969ebcbb26c2c86695f8c9254e2aca9dc8e513b
Vlad <furdanno@mail.ru>
2020-10-04 16:49:19 +0300

    Пересечение двух массивов

--- Big O.php
+++ Big O.php
@@ -1,15 +1,13 @@
 <?php
 function getIntersectionOfSortedArray(array $mas1,array $mas2)
 {
-        $res = [];
-        foreach ($mas1 as $chi1) {
-            foreach ($mas2 as $chi2) {
-                if ($chi1 === $chi2) {
-                    $res[] = $chi1;
-                }
+    $res = [];
+    foreach ($mas1 as $chi1) {
+        foreach ($mas2 as $chi2) {
+            if ($chi1 === $chi2) {
+                $res[] = $chi1;
             }
         }
+    }
     return $res;
 }
-
-print_r(getIntersectionOfSortedArray([11,24,-2],[3,5,10,-2]));
